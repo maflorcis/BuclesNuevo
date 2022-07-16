def9604bad3440af95e5850db5db1ec0a2c518cc
floralonso <floralonso@gmail.com>
2022-07-16 17:30:55 -0300

    ejercicios1a7

---
+++ js/buclesEjercicio1.js
@@ -0,0 +1,11 @@
+let edad = parseInt(prompt("Escribe tu edad"));
+
+	if (edad >= 18) {
+		//IMPRIME EN PANTALLA EL MENSAJE
+		document.write("Puedes conducir");
+	}
+//SINO
+else {
+	//MUESTRA UN MENSAJE
+	document.write("Introduce un numero válido");
+}
